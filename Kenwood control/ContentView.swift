397cf66ee92b7138f5a0db1ea933c81e940b000a
Justin Mann <w9fyi@me.com>
2026-02-11 16:25:34 -0600

    Initial Commit

---
+++ Kenwood control/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Kenwood control
+//
+//  Created by justin Mann on 2/11/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Kenwood control/Kenwood_controlApp.swift
@@ -0,0 +1,17 @@
+//
+//  Kenwood_controlApp.swift
+//  Kenwood control
+//
+//  Created by justin Mann on 2/11/26.
+//
+
+import SwiftUI
+
+@main
+struct Kenwood_controlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
